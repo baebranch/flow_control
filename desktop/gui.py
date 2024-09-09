109410897e3dbd2833bb8b7bbf76fc769f2a227f
Brian Branch <baebranch@outlook.com>
2024-09-09 11:31:22 -0400

    Build changes for version 0.01

--- desktop/gui.py
+++ desktop/gui.py
@@ -1,10 +1,14 @@
 import time
-import webview 
+import webview
+from threading import Thread
+
+from server.server import run_app
 
 
 def splash_close(window):
-  """ Close splash screen after 7 seconds """
-  # Start servers and backend processes
+  """ Start servers and close splash screen after 7 seconds """
+  server = Thread(target=run_app, daemon=True)
+  server.start()
 
   # Close splash screen
   time.sleep(7)
@@ -12,10 +16,10 @@ def splash_close(window):
 
 
 # Create splash screen
-splash_screen = webview.create_window('Flow Control', html=open('desktop/splash.html', 'r').read(), frameless=True, width=480, height=307)
+splash_screen = webview.create_window('Flow Control', html=(open('splash.html', 'r').read()), frameless=True, width=480, height=307)
 webview.start(splash_close, (splash_screen,), http_server=False)
 
 # Open main window 
-# webview.create_window('Flow Control', 'http://localhost:1212/') 
-webview.create_window('Flow Control', html='./index.html')
+window = webview.create_window('Flow Control', 'http://flow.localhost:1212/') 
+
 webview.start() 
